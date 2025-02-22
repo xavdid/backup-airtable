7e79558be4bf41a3cc22c319a5ecd137134beeb7
David Brownman <oss@xavd.id>
2025-02-22 15:44:06 -0800

    streamline tests

--- tests/test_backup_airtable.py
+++ tests/test_backup_airtable.py
@@ -1,10 +1,10 @@
 import json
 from pathlib import Path
-from typing import Callable, Optional, TypedDict
+from typing import Callable, Optional, Protocol, TypedDict
 from unittest.mock import patch
 
 import pytest
-from click.testing import CliRunner
+from click.testing import CliRunner, Result
 from pytest_httpx import HTTPXMock
 
 from backup_airtable.cli import build_client, cli, load_all_items
@@ -23,11 +23,11 @@ class BaseInfo(TypedDict):
 type BasesFn = Callable[[], list[BaseInfo]]
 
 
-# todo: use this as a helper, but provide a fixture to get the clean copy directly
 @pytest.fixture
 def get_bases() -> BasesFn:
     """
-    use a function so that every use of this fixture is brand new and can be mutated safely
+    This is the full, final representation of the DB dump.
+    In a function so other fixtures can modify it safely, but also available directly as a fixture.
     """
 
     def _build_bases() -> list[BaseInfo]:
@@ -232,20 +232,25 @@ def get_bases() -> BasesFn:
 
 
 @pytest.fixture
-def mock_bases(httpx_mock, get_bases: BasesFn):
+def bases(get_bases) -> list[BaseInfo]:
+    return get_bases()
+
+
+@pytest.fixture
+def mock_bases(httpx_mock, bases):
     httpx_mock.add_response(
         url="https://api.airtable.com/v0/meta/bases",
         match_headers={
             "Authorization": "Bearer pat123.456",
             "user-agent": "backup-airtable",
         },
-        json={"bases": [b["info"] for b in get_bases()]},
+        json={"bases": [b["info"] for b in bases]},
     )
 
 
 @pytest.fixture
-def mock_tables(httpx_mock: HTTPXMock, get_bases: BasesFn, mock_bases):  # noqa: ARG001
-    for b in get_bases():
+def mock_tables(httpx_mock: HTTPXMock, bases, mock_bases):  # noqa: ARG001
+    for b in bases:
         httpx_mock.add_response(
             url=f"https://api.airtable.com/v0/meta/bases/{b['info']['id']}/tables",
             match_headers={"Authorization": "Bearer pat123.456"},
@@ -266,7 +271,7 @@ def mock_records(httpx_mock, mock_tables, get_bases: BasesFn):  # noqa: ARG001
                 records = []
 
                 for r in t["records"]:
-                    # comments are _never_ in the root response
+                    # comments[] are _never_ in the root response, but may be fetched and merged later
                     comments = r.pop("comments")
                     if with_comments and comments:
                         httpx_mock.add_response(
@@ -297,75 +302,101 @@ def bases_no_comments(get_bases: BasesFn):
     return bases
 
 
-def test_version():
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        result = runner.invoke(cli, ["--version"])
-        assert result.exit_code == 0
-        assert result.output.startswith("cli, version ")
+class InvokeFn(Protocol):
+    def __call__(
+        self,
+        args: Optional[list[str]] = None,
+        *,
+        expected_status=0,
+        backup_dir: Optional[list[str]] = None,
+    ) -> Result: ...
 
 
-def test_full_backup(tmpdir, mock_records, bases_no_comments: list[BaseInfo]):
-    bases = bases_no_comments
+@pytest.fixture
+def invoke(tmp_path, monkeypatch) -> InvokeFn:
+    def _run(
+        args: Optional[list[str]] = None,
+        expected_status=0,
+        backup_dir: Optional[list[str]] = None,
+    ):
+        if args is None:
+            args = []
+        # use a list here so we can disappear it with an unpack
+        if backup_dir is None:
+            backup_dir = [str(tmp_path)]
+
+        runner = CliRunner()
+
+        monkeypatch.chdir(tmp_path)
+
+        # write to tmp_path directly so we don't have to get the subfolder in tests
+        result = runner.invoke(
+            cli, [*backup_dir, *args], env={"AIRTABLE_TOKEN": "pat123.456"}
+        )
+
+        if result.exit_code != expected_status:
+            # much easier to debug this way
+            print(result.output)
+
+        assert result.exit_code == expected_status
+
+        return result
+
+    return _run
+
+
+def test_version(invoke: InvokeFn):
+    result = invoke(["--version"])
+    assert result.output.startswith("cli, version ")
+
+
+def test_full_backup(
+    tmp_path, mock_records, bases_no_comments: list[BaseInfo], invoke: InvokeFn
+):
     mock_records()
-
-    runner = CliRunner()
-    result = runner.invoke(cli, [str(tmpdir)], env={"AIRTABLE_TOKEN": "pat123.456"})
-    assert result.exit_code == 0
+    invoke()
 
     assert (
         json.loads(
-            Path(tmpdir / "Base the First" / "Cool Table" / "schema.json").read_text()
+            Path(tmp_path, "Base the First", "Cool Table", "schema.json").read_text()
         )
-        == bases[0]["tables"][0]["info"]
+        == bases_no_comments[0]["tables"][0]["info"]
     )
     assert json.loads(
-        Path(tmpdir / "Base the First" / "Cool Table" / "records.json").read_text()
+        Path(tmp_path, "Base the First", "Cool Table", "records.json").read_text()
     ) == [
-        bases[0]["tables"][0]["records"][1],
-        bases[0]["tables"][0]["records"][0],
+        bases_no_comments[0]["tables"][0]["records"][1],
+        bases_no_comments[0]["tables"][0]["records"][0],
     ]  # sorted by creation date
     assert (
         json.loads(
             Path(
-                tmpdir / "Base the First" / "Tough- name? | neat" / "schema.json"
+                tmp_path, "Base the First", "Tough- name? | neat", "schema.json"
             ).read_text()
         )
-        == bases[0]["tables"][1]["info"]
+        == bases_no_comments[0]["tables"][1]["info"]
     )
     assert (
         json.loads(
-            Path(tmpdir / "Base the Second" / "Cool Table" / "schema.json").read_text()
+            Path(tmp_path, "Base the Second", "Cool Table", "schema.json").read_text()
         )
-        == bases[1]["tables"][0]["info"]
+        == bases_no_comments[1]["tables"][0]["info"]
     )
     assert (
         json.loads(
-            Path(tmpdir / "Base the Second" / "Cool Table" / "records.json").read_text()
+            Path(tmp_path, "Base the Second", "Cool Table", "records.json").read_text()
         )
-        == bases[1]["tables"][0]["records"]
+        == bases_no_comments[1]["tables"][0]["records"]
     )
 
 
-def test_full_backup_with_comments(
-    tmpdir, mock_records, get_bases: BasesFn, httpx_mock: HTTPXMock
-):
-    bases = get_bases()
+def test_full_backup_with_comments(tmp_path, mock_records, bases, invoke: InvokeFn):
     mock_records(with_comments=True)
 
-    runner = CliRunner()
-    result = runner.invoke(
-        cli,
-        [str(tmpdir), "--include-comments"],
-        env={"AIRTABLE_TOKEN": "pat123.456"},
-    )
-    print(result.stdout)
-    print(httpx_mock.get_requests())
-
-    assert result.exit_code == 0
+    invoke(["--include-comments"])
 
     assert json.loads(
-        Path(tmpdir / "Base the First" / "Cool Table" / "records.json").read_text()
+        Path(tmp_path, "Base the First", "Cool Table", "records.json").read_text()
     ) == [
         bases[0]["tables"][0]["records"][1],
         bases[0]["tables"][0]["records"][0],
@@ -397,69 +428,56 @@ def test_full_backup_with_comments(
     ]
 
 
-def test_skipping_tables(tmpdir, mock_records, bases_no_comments):
+def test_skipping_tables(tmp_path, mock_records, bases_no_comments, invoke: InvokeFn):
     mock_records(["tbl123", "tbl789"])
-    bases = bases_no_comments
-
-    runner = CliRunner()
-    result = runner.invoke(
-        cli,
+    invoke(
         [
-            str(tmpdir),
-            "--airtable-token",
-            "pat123.456",
             "--ignore_table",
             "tbl123",
             "--ignore_table",
             "tbl789",
-        ],
+        ]
     )
-    assert result.exit_code == 0
 
-    assert not Path(tmpdir / "Base the First" / "Cool Table").exists()
+    assert not Path(tmp_path, "Base the First", "Cool Table").exists()
     assert (
         json.loads(
             Path(
-                tmpdir / "Base the First" / "Tough- name? | neat" / "schema.json"
+                tmp_path, "Base the First", "Tough- name? | neat", "schema.json"
             ).read_text()
         )
-        == bases[0]["tables"][1]["info"]
+        == bases_no_comments[0]["tables"][1]["info"]
     )
     assert (
         json.loads(
             Path(
-                tmpdir / "Base the First" / "Tough- name? | neat" / "records.json"
+                tmp_path, "Base the First", "Tough- name? | neat", "records.json"
             ).read_text()
         )
-        == bases[0]["tables"][1]["records"]
+        == bases_no_comments[0]["tables"][1]["records"]
     )
-    assert not Path(tmpdir / "Base the Second" / "Cool Table").exists()
+    assert not Path(tmp_path, "Base the Second", "Cool Table").exists()
 
 
 @pytest.mark.freeze_time("2024-04-24")
-def test_default_path(tmpdir, mock_records):
+def test_default_path(tmp_path, mock_records, invoke: InvokeFn):
     mock_records()
-    runner = CliRunner()
-    with runner.isolated_filesystem(str(tmpdir)) as wd:
-        result = runner.invoke(cli, env={"AIRTABLE_TOKEN": "pat123.456"})
-        assert result.exit_code == 0
+    invoke(backup_dir=[])  # use the CLI's default location
 
-        assert Path(wd, "airtable-backup-2024-04-24").exists()
+    assert Path(tmp_path, "airtable-backup-2024-04-24").exists()
 
 
-def test_airtable_export_401_error(httpx_mock):
+def test_airtable_export_401_error(httpx_mock, invoke: InvokeFn):
     httpx_mock.add_response(status_code=401)
-    runner = CliRunner()
-    result = runner.invoke(cli, ["--airtable-token", "pat123.456"])
+    result = invoke(expected_status=1)
     assert result.exit_code == 1
     assert "401 Unauthorized" in result.output
     assert "HINT: Ensure" not in result.output
 
 
-def test_airtable_bad_permissions(httpx_mock):
+def test_airtable_bad_permissions(httpx_mock, invoke: InvokeFn):
     httpx_mock.add_response(status_code=403)
-    runner = CliRunner()
-    result = runner.invoke(cli, ["--airtable-token", "pat123.456"])
+    result = invoke(expected_status=1)
     assert result.exit_code == 1
     assert "403 Forbidden" in result.output
     assert "HINT: Ensure" in result.output
